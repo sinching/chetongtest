cda43f6a1027d94947e496216abb544e5f20d19c
ching <liangsq@chetong.net>
2016-06-01 15:36:27 +0800

    pyhton Test

---
+++ license.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import os 
+import sys 
+import time
+print os.path
+print sys.path
+print 'hello world!'
+for i in range(1,21):
+    print i
+print "done"    
